27090ce51ebbeeb27d191faea30c8ed13e919078
Rodrigo Mecheri <rodrigobme@hotmail.com>
2023-04-04 08:12:22 -0300

    Commit

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links=("id", "nome")
+    search_fields=("nome",)
+    list_filter=('categoria','publicada')
+    list_per_page=5
+    list_editable=("publicada",)
+
+admin.site.register(Fotografia, ListandoFotografias)
